8b908e3fce9cb41b1f8c3d3dc2eba38c85e2dbf5
Ninoh van Dijke <47174@hoornbeeck.nl>
2024-02-06 14:06:54 +0100

    Delete en Comments toegevoegd

--- event_v3/resources/views/events/index.blade.php
+++ event_v3/resources/views/events/index.blade.php
@@ -7,13 +7,12 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <h2 class="font-bold text-2xl mb-4 ml-16">Evenementen</h2> <!-- Titel toegevoegd -->
+            <h2 class="font-bold text-2xl mb-4 ml-16">Evenementen</h2>
 
+
+        {{--Show all & Create buttons--}}
             <div x-data="{ openEventIndex: null }" x-cloak class="flex bg-white  ml-8 mr-8 shadow-xl rounded-xl">
 
-{{--                <div class="flex m-4 font-sans font-bold text-xl">--}}
-{{--                    <p>Evenementen </p>--}}
-{{--                </div>--}}
                 <div class="absolute flex m-4 ml-8 font-sans font-bold text-xl ">
                     <button @click="openEventIndex === null ? openEventIndex = {{ $events }} : openEventIndex = null" type="button" class="w-auto bg-white tracking-wide text-gray-800 font-bold rounded border-b-2 border-blue-500 hover:border-blue-600 hover:bg-blue-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
                         <span x-show="openEventIndex === null">Show all events</span>
@@ -22,9 +21,11 @@
                 </div>
                 <div class="absolute m-4 ml-8 mt-16 font-sans font-bold text-xl ">
                     <a href="{{ route('events.create') }}" class="w-auto h-10 bg-white tracking-wide text-gray-800 font-bold rounded border-b-2 border-green-500 hover:border-green-600 hover:bg-green-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">Create event</a>
-
                 </div>
-                @foreach($events->take(4) as $event)
+        {{--Show all & Create buttons--}}
+
+        {{--First 4 events visible--}}
+            @foreach($events->take(4) as $event)
                     <div class=" w-full sm:w-1/2 md:w-1/4 px-2 mb-4 mt-20">
                         <div class="my-6 p-6  border-b border-gray-200 shadow-sm sm:rounded-lg">
                             <div class="event-item rounded-xl p-0.5">
@@ -40,23 +41,34 @@
                                         <div class="font-sans">{{ \Carbon\Carbon::parse($event->event_date)->format('d M Y') }} {{$event->start_time}}</div>
                                     </div>
                                 </a>
+        {{--First 4 events visible--}}
+
+        {{--DELETE BUTTON--}}
                                     <button type="button" onclick="deleteOpenConfirmationPopup('{{ $event->id }}')" class="w-auto h-8 bg-white tracking-wide text-gray-800 font-bold rounded border-b-2 border-red-500 hover:border-red-600 hover:bg-red-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">Delete event</button>
 
                                     <div id="confirmationPopup-{{ $event->id }}" class="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex items-center justify-center" style="display: none;">
                                         <div class="bg-white p-8 rounded-md shadow-md">
-                                            <p class="text-lg mb-4">Are you sure you want to delete this event?</p>
-                                            <div class="flex justify-between">
-                                                <button onclick="deleteCloseConfirmationPopup('{{ $event->id }}')" class="bg-gray-500 text-white px-4 py-2 rounded mr-2">Cancel</button>
+                                            <div class="text-center p-5 flex-auto justify-center">
 
-                                                <form id="deleteForm-{{ $event->id }}" method="POST" action="{{ route('events.destroy', $event->id) }}" class="inline">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="bg-red-500 text-white px-4 py-2 rounded">Confirm</button>
-                                                </form>
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="w-16 h-16 flex items-center text-red-500 mx-auto" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                                </svg>
+                                                <h2 class="text-xl font-bold py-4 ">Are you sure?</h2>
+                                                    <p class="text-sm text-gray-500 px-8">Do you really want to delete your account? This process cannot be undone</p>
+
+                                                <div class="p-3  mt-2 text-center space-x-4 md:block">
+                                                    <button onclick="deleteCloseConfirmationPopup('{{ $event->id }}')" class="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-full hover:shadow-lg hover:bg-gray-100">Cancel</button>
+
+                                                    <form id="deleteForm-{{ $event->id }}" method="POST" action="{{ route('events.destroy', $event->id) }}" class="inline">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" class="mb-2 md:mb-0 bg-red-500 border border-red-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-red-600">Delete</button>
+                                                    </form>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-
+        {{--DELETE BUTTON--}}
                         </div>
                     </div>
                 @endforeach
@@ -64,7 +76,7 @@
 
 
 
-
+        {{--Rest of the Events--}}
                 <div x-show="openEventIndex !== null" x-cloak class="relative flex">
                     @foreach($events->slice(4) as $event)
                         <div >
@@ -83,14 +95,15 @@
                             </div>
                         </div>
                     @endforeach
-
                 </div>
+        {{--Rest of the Events--}}
 
             </div>
         </div>
     </div>
-
 </x-app-layout>
+
+
 <script>
     function deleteOpenConfirmationPopup(deleteId) {
         // Show the confirmation popup
